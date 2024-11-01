b5b894576358a86aa2ab9604988c509eacb220f6
Benjamin Lieser <benjamin.lieser@mpinat.mpg.de>
2024-11-01 17:44:32 +0100

    move random tree util

--- felsenstein_impl/src/lib.rs
+++ felsenstein_impl/src/lib.rs
@@ -9,6 +9,9 @@ mod preprocessing;
 mod train;
 mod tree;
 
+use rand::distributions::uniform::SampleUniform;
+use rand::prelude::Distribution;
+use rand::seq::SliceRandom;
 pub use train::InferenceResultParam;
 
 use crate::data_types::*;
@@ -36,4 +39,43 @@ impl<F: FloatTrait, const DIM : usize> FTreeBackend<F, DIM> {
     pub fn infer(&self, s: Vec<na::SMatrix<F, DIM, DIM>>, sqrt_pi: Vec<na::SVector<F, DIM>>) -> InferenceResultParam<F, DIM> {
         train_parallel_param_unpaired(&self.leaf_log_p, &s, &sqrt_pi, &self.tree, &self.distances)
     }
+}
+
+pub fn random_tree_top(num_leaf: u32) -> Vec<i32> {
+    let mut parents = vec![-2;num_leaf as usize];
+    let mut orphans : Vec<u32> = (0..num_leaf).collect();
+
+    let mut rng = rand::thread_rng();
+
+    while orphans.len() > 3 {
+        // This is inefficient, but it's not a bottleneck
+        orphans.shuffle(&mut rng);
+        let parent_id = parents.len();
+        parents.push(-2);
+        let sib1 = orphans.pop().unwrap();
+        let sib2 = orphans.pop().unwrap();
+
+        parents[sib1 as usize] = parent_id as i32;
+        parents[sib2 as usize] = parent_id as i32;
+
+        orphans.push(parent_id as u32);
+
+    }
+
+    let root_id = parents.len();
+    let sib1 = orphans.pop().unwrap();
+    let sib2 = orphans.pop().unwrap();
+    let sib3 = orphans.pop().unwrap();
+    parents[sib1 as usize] = root_id as i32;
+    parents[sib2 as usize] = root_id as i32;
+    parents[sib3 as usize] = root_id as i32;
+
+    parents.push(-1);
+
+    parents
+}
+
+pub fn random_dist<F : FloatTrait + SampleUniform>(num_nodes : u32) -> Vec<F> {
+    let dist = rand::distributions::Uniform::new::<F, F>(FloatTrait::from_f64(0.1), FloatTrait::from_f64(1.0));
+    (0..num_nodes).map(|_| dist.sample(&mut rand::thread_rng())).collect()
 }
--- felsenstein_impl/src/main.rs
+++ felsenstein_impl/src/main.rs
@@ -1,50 +1,7 @@
 #![allow(non_snake_case)]
-#![feature(portable_simd)]
 
-
-use rand::{distributions::uniform::SampleUniform, prelude::Distribution, seq::SliceRandom};
 extern crate nalgebra as na;
 
-use felsenstein_impl::data_types::*;
-
-pub fn gen_tree_top(num_leaf: u32) -> Vec<i32> {
-    let mut parents = vec![-2;num_leaf as usize];
-    let mut orphans : Vec<u32> = (0..num_leaf).collect();
-
-    let mut rng = rand::thread_rng();
-
-    while orphans.len() > 3 {
-        // This is inefficient, but it's not a bottleneck
-        orphans.shuffle(&mut rng);
-        let parent_id = parents.len();
-        parents.push(-2);
-        let sib1 = orphans.pop().unwrap();
-        let sib2 = orphans.pop().unwrap();
-
-        parents[sib1 as usize] = parent_id as i32;
-        parents[sib2 as usize] = parent_id as i32;
-
-        orphans.push(parent_id as u32);
-
-    }
-
-    let root_id = parents.len();
-    let sib1 = orphans.pop().unwrap();
-    let sib2 = orphans.pop().unwrap();
-    let sib3 = orphans.pop().unwrap();
-    parents[sib1 as usize] = root_id as i32;
-    parents[sib2 as usize] = root_id as i32;
-    parents[sib3 as usize] = root_id as i32;
-
-    parents.push(-1);
-
-    parents
-}
-
-pub fn gen_dist<F : FloatTrait + SampleUniform>(num_nodes : u32) -> Vec<F> {
-    let dist = rand::distributions::Uniform::new::<F, F>(FloatTrait::from_f64(0.1), FloatTrait::from_f64(1.0));
-    (0..num_nodes).map(|_| dist.sample(&mut rand::thread_rng())).collect()
-}
 
 use f64 as Float;
 const DIM : usize = 20;
@@ -54,8 +11,8 @@ pub fn main() {
     let L = 30;
 
 
-    let parents = gen_tree_top(num_leaf);
-    let dist = gen_dist::<Float>(parents.len() as u32);
+    let parents = felsenstein_impl::random_tree_top(num_leaf);
+    let dist = felsenstein_impl::random_dist::<Float>(parents.len() as u32);
 
     let leaf_log_p : Vec<Vec<na::SVector<Float, DIM>>> = (0..L).map(|_| {
         (0..num_leaf).map(|_| {
