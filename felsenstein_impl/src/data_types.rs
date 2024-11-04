fc6ce09ee7ef09d93221b0df2520933bbe40f096
Benjamin Lieser <benjamin.lieser@mpinat.mpg.de>
2024-11-04 15:40:25 +0100

    refactor simd

--- felsenstein_impl/src/data_types.rs
+++ felsenstein_impl/src/data_types.rs
@@ -1,5 +1,3 @@
-
-
 use std::{iter::Sum, simd::num::SimdFloat};
 
 use logsumexp::LogSumExp;
@@ -23,10 +21,11 @@ where
     const EPS_DIV: Self; // Minimum value for sqrt_pi
     fn logsumexp<'a, I: Iterator<Item = &'a Self>>(iter: I) -> Self;
     fn from_f64(f: f64) -> Self;
-    fn libm_exp(self) -> Self;
-    fn vec_exp<const N : usize>(x : &mut [Self;N]);
-    fn vec_logsumexp<const N : usize>(x : &[Self;N]) -> Self;
+    fn scalar_exp(self) -> Self;
+    fn vec_exp<const N: usize>(x: &mut [Self; N]);
+    fn vec_logsumexp<const N: usize>(x: &[Self; N]) -> Self;
 }
+
 impl FloatTrait for f32 {
     fn logsumexp<'a, I: Iterator<Item = &'a Self>>(iter: I) -> Self {
         LogSumExp::ln_sum_exp(iter)
@@ -36,13 +35,23 @@ impl FloatTrait for f32 {
     }
     const EPS_LOG: Self = 1e-15;
     const EPS_DIV: Self = 1e-10;
-    fn libm_exp(self) -> Self {
-        libm::expf(self)
+    fn scalar_exp(self) -> Self {
+        sleef::f32::exp_u10(self)
     }
-    fn vec_exp<const N : usize>(_x : &mut [Self;N]) {
-        panic!("Not implemented");
+    fn vec_exp<const N: usize>(x: &mut [Self; N]) {
+        let blocks = N / 8;
+
+        for i in 0..blocks {
+            let a = simd::f32x8::from_slice(&x[i * 8..]);
+            let b = sleef::f32x::exp_u10(a);
+            simd::f32x8::copy_to_slice(b, &mut x[i * 8..]);
+        }
+
+        for i in blocks * 8..N {
+            x[i] = x[i].scalar_exp();
+        }
     }
-    fn vec_logsumexp<const N : usize>(_x : &[Self;N]) -> Self {
+    fn vec_logsumexp<const N: usize>(_x: &[Self; N]) -> Self {
         panic!("Not implemented");
     }
 }
@@ -55,46 +64,46 @@ impl FloatTrait for f64 {
     }
     const EPS_LOG: Self = 1e-100;
     const EPS_DIV: Self = 1e-10;
-    fn libm_exp(self) -> Self {
+    fn scalar_exp(self) -> Self {
         sleef::f64::exp_u10(self)
     }
-    fn vec_exp<const N : usize>(x : &mut [Self;N]) {
+    fn vec_exp<const N: usize>(x: &mut [Self; N]) {
         let blocks = N / 4;
 
         for i in 0..blocks {
-            let a = simd::f64x4::from_slice(&x[i*4..]);
+            let a = simd::f64x4::from_slice(&x[i * 4..]);
             let b = sleef::f64x::exp_u10(a);
-            simd::f64x4::copy_to_slice(b, &mut x[i*4..]);
+            simd::f64x4::copy_to_slice(b, &mut x[i * 4..]);
         }
 
-        for i in blocks*4..N {
-            x[i] = x[i].libm_exp();
+        for i in blocks * 4..N {
+            x[i] = x[i].scalar_exp();
         }
     }
-    fn vec_logsumexp<const N : usize>(x : &[Self;N]) -> Self {
+    fn vec_logsumexp<const N: usize>(x: &[Self; N]) -> Self {
         let blocks = N / 4;
-
-        //TODO handle other cases
-        assert!(N % 4 == 0);
 
         let mut max = simd::f64x4::splat(f64::NEG_INFINITY);
         for i in 0..blocks {
-            let a = simd::f64x4::from_slice(&x[i*4..]);
+            let a = simd::f64x4::from_slice(&x[i * 4..]);
             max = max.simd_max(a);
         }
+        let last_elements = simd::f64x4::load_or(&x[blocks * 4..], simd::f64x4::splat(f64::NEG_INFINITY));
+        max = max.simd_max(last_elements);
 
-        let max = max[0].max(max[1].max(max[2].max(max[3])));
+        let max = max.reduce_max();
 
         let mut sum = simd::f64x4::splat(0.0);
         for i in 0..blocks {
-            let a = simd::f64x4::from_slice(&x[i*4..]);
+            let a = simd::f64x4::from_slice(&x[i * 4..]);
             let b = a - simd::f64x4::splat(max);
             let c = sleef::f64x::exp_u10(b);
             sum += c;
         }
 
+        let last_elements = simd::f64x4::load_or_default(&x[blocks * 4..]);
+        sum += sleef::f64x::exp_u10(last_elements - simd::f64x4::splat(max));
 
-        return max + (sum[0] + sum[1] + sum[2] + sum[3]).ln();
-
+        return max + (sum.reduce_sum()).ln();
     }
 }
