709610b33d517c8e72a3207a108f1716af678488
Benjamin Lieser <benjamin.lieser@mpinat.mpg.de>
2024-11-06 11:59:35 +0100

    rename to phylo_grad

--- phylo_grad/src/main.rs
+++ phylo_grad/src/main.rs
@@ -1,28 +1,29 @@
 #![allow(non_snake_case)]
 
 extern crate nalgebra as na;
-
+use phylo_grad as lib;
 
 use f64 as Float;
 const DIM : usize = 20;
 
+/// Runs the algorithm on a random tree in pure Rust, this makes debugging and profiling easier.
 pub fn main() {
     let num_leaf = 100;
     let L = 30;
 
 
-    let parents = felsenstein_impl::random_tree_top(num_leaf);
-    let dist = felsenstein_impl::random_dist::<Float>(parents.len() as u32);
+    let parents = lib::random_tree_top(num_leaf);
+    let dist = lib::random_dist::<Float>(parents.len() as u32);
 
     let leaf_log_p : Vec<Vec<na::SVector<Float, DIM>>> = (0..L).map(|_| {
         (0..num_leaf).map(|_| {
             let init = na::SVector::<Float, DIM>::from_iterator((0..DIM).map(|_| rand::random()));
-            let dist = felsenstein_impl::backward::softmax(&init);
+            let dist = lib::backward::softmax(&init);
             dist.map(Float::ln)
         }).collect()
     }).collect();
 
-    let backend = felsenstein_impl::FelsensteinTree::new(parents, dist, leaf_log_p);
+    let backend = lib::FelsensteinTree::new(parents, dist, leaf_log_p);
     
     let s : Vec<_> = (0..L).map(|_| {
         na::SMatrix::<Float, DIM, DIM>::from_iterator((0..DIM*DIM).map(|_| rand::random::<Float>().exp()))
@@ -30,7 +31,7 @@ pub fn main() {
 
     let sqrt_pi : Vec<_> = (0..L).map(|_| {
         let init = na::SVector::<Float, DIM>::from_iterator((0..DIM).map(|_| rand::random()));
-        let dist = felsenstein_impl::backward::softmax(&init);
+        let dist = lib::backward::softmax(&init);
         dist.map(Float::sqrt)
     }).collect();
 
