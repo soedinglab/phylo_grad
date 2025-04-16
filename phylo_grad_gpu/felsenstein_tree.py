c5f921593956968724038a0c1cea1cdb4f3a7f28
Benjamin Lieser <benjamin.lieser@mpinat.mpg.de>
2025-04-16 16:02:36 +0200

    first jax prototype naive

---
+++ phylo_grad_gpu/felsenstein_tree.py
@@ -0,0 +1,61 @@
+import jax
+import jax.numpy as jnp
+
+class FelsensteinNode:
+    def __init__(self, idx: int) -> None:
+        self.children = []
+        self.idx = idx
+
+    def get_computation_plan(self) -> list[tuple[int, int]]:
+        """
+        Returns a computation plan for the Felsenstein algorithm.
+        The plan is a list of tuples, where each tuple (child, parent) means that the log_p_transformation of child will be added to the log_p of parent
+        """
+        plan = []
+        for child in self.children:
+            plan += child.get_computation_plan()
+            plan.append((child.idx, self.idx))
+        
+        return plan
+
+def log_p_transformation(t : float, rate_matrix : jnp.ndarray, log_p : jnp.ndarray) -> jnp.ndarray:
+    matrix_exp = jax.scipy.linalg.expm(t * rate_matrix)
+    log_trans = jnp.log(matrix_exp)
+    new_log_p = jax.scipy.special.logsumexp(jnp.expand_dims(log_p, 0) + log_trans, axis=1)
+    return new_log_p
+
+class FelsensteinTree:
+    def __init__(self, parent_list : jnp.ndarray, leaf_log_p : jnp.ndarray, distance_threshold : float = 1e-4) -> None:
+        
+        self.nodes = [FelsensteinNode(idx) for idx in range(len(parent_list))]
+        
+        self.root = None
+        
+        branch_lengths = []
+        for child_idx, (parent_idx, branch_length) in enumerate(parent_list):
+            if parent_idx == -1:
+                self.root = self.nodes[child_idx]
+            else:
+                self.nodes[parent_idx].children.append(self.nodes[child_idx])
+            branch_lengths.append(max(branch_length, distance_threshold))
+        
+        assert(self.root is not None), "Tree has no root"
+        
+        self.branch_lengths = jnp.array(branch_lengths)
+        plan = self.root.get_computation_plan()
+        self.computation_plan = jnp.array(plan, dtype=jnp.int32)
+    
+        internal_nodes = len(parent_list) - leaf_log_p.shape[1]
+    
+        # Extent the leaf_log_p to include the internal nodes
+        self.global_log_p = jnp.pad(leaf_log_p, ((0, 0), (0, internal_nodes), (0, 0)), mode='constant', constant_values=0.0)
+        
+    def log_p_single_column(self, rate_matrix : jnp.ndarray, global_log_p) -> jnp.ndarray:
+        
+        def do_step(idx, current_global_log_p):
+            child, parent = self.computation_plan[idx]
+            branch_length = self.branch_lengths[child]
+            new_log_p = log_p_transformation(branch_length, rate_matrix, current_global_log_p[child])
+            return current_global_log_p.at[parent].add(new_log_p)
+        # Loop over the computation plan
+        return jax.lax.fori_loop(0, self.computation_plan.shape[0], do_step, global_log_p)
