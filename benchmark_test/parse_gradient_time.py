e21d1614b009ffe01b022651e3405b97732bf43c
Benjamin Lieser <benjamin.lieser@stud.uni-goettingen.de>
2025-11-05 22:11:35 +0100

    parse also raxml stuff

--- benchmark_test/parse_gradient_time.py
+++ benchmark_test/parse_gradient_time.py
@@ -9,8 +9,14 @@ pattern = r"GradientTime=(\d+)"
 matches = re.findall(pattern, content)
 
 total_runtime = re.search(r"Total wall-clock time used: ([\d\.]+) sec", content)
+if total_runtime is None: # raxml log
+    total_runtime = re.search(r"Elapsed time: ([\d\.]+) seconds", content)
+best_score = re.search(r"BEST SCORE FOUND : ([\d\.\-]+)", content)
+if best_score is None: # raxml log
+    best_score = re.search(r"Final LogLikelihood: ([\d\.\-]+)", content)
 
 print("Number of gradient calculations:", len(matches))
 print("Average gradient time (ms):", sum(map(float, matches)) / len(matches) / 1000.0)
 print("Total gradient time (s):", sum(map(float, matches)) / 1_000_000.0)
-print("Total wall-clock time used (s):", total_runtime.group(1))
+print("Total wall-clock time used (s):", total_runtime.group(1) if total_runtime else "N/A")
+print("Best log likelihood found:", best_score.group(1) if best_score else "N/A")
